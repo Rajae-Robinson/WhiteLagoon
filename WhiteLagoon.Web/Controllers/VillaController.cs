74ea1e35a40129bb524a186e972850db9f7fdaa5
Rajae Robinson <rajrobinson18@gmail.com>
2024-09-24 09:09:32 -0500

    add Villa create and view pages

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Web.Controllers
@@ -15,6 +16,22 @@ namespace WhiteLagoon.Web.Controllers
         public IActionResult Index()
         {
             var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if(ModelState.IsValid) {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");            
+            }
             return View();
         }
 
